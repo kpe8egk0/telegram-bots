198f85b58e1b670ed57b5d2a6943235541f4e53f
ilya.kiselev <ilya.kiselev@hotmail.com>
2016-09-07 16:25:24 +0300

    bot_commands test

--- transnow_bot/test_bot2.php
+++ transnow_bot/test_bot2.php
@@ -2,7 +2,8 @@
 // https://api.telegram.org/bot261062241:AAHYU1rMeyMW4I0z6bxrwP3HpeaJKLVNXxs/setWebhook?url=https://transnow-ironyman.rhcloud.com/transnow_bot/test_bot2.php
 $trans = array();
 $source = 'yandex';
-$yandex_key = 'dict.1.1.20160819T080857Z.a21f9f5c92e0e7b9.ab24906e2b9b24a62bede201ca3067abadaf5752';
+$yandex_dict_key = 'dict.1.1.20160819T080857Z.a21f9f5c92e0e7b9.ab24906e2b9b24a62bede201ca3067abadaf5752';
+$yandex_trans_key = 'trnsl.1.1.20160906T144940Z.7b9bdff453462ecd.bcabb5b47a3afe432e57931793362ad73e47898f';
 
 $access_token = '261062241:AAHYU1rMeyMW4I0z6bxrwP3HpeaJKLVNXxs';
 $api = 'https://api.telegram.org/bot' . $access_token;
@@ -13,15 +14,103 @@ $output = json_decode($input_json, TRUE);
 $chat_id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $username = $output['message']['from']['first_name'].' '.$output['message']['from']['last_name'];
+$ifcommand = $output['message']['entities']['type'];
 $lang = 'ru-en';
 
-$url = sprintf('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=%s&lang=%s&text=%s', $yandex_key, $lang, $message);
-$data = json_decode(file_get_contents($url));
-for ($i = 0; $i<=4; $i++) {
-    $trans[$i] = $data->def[0]->tr[$i]->text;
-}
-$transfiltered = array_filter ($trans);
-$reply = 'Dear '.$username.'! The word "'.$message.'" translates like: '.implode(', ', $transfiltered).'. The translated array length is '.count($transfiltered).'.';
+$reply = incoming($source, $message, $yandex_dict_key, $yandex_trans_key, $ifcommand, $lang, $ui);
 file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($reply));
-echo $reply;
 exit();
+
+//----------------------------------------------- Functions ----------------------------------------------
+
+function incoming($source, $message, $yandex_dict_key, $yandex_trans_key, $ifcommand, $lang, $ui)
+{
+    if ($ifcommand ==  'bot_command')
+    {
+        $array_msg = explode( ' ', $message);
+        $article = getArticleFromSource($source, $lang, $array_msg[1], $yandex_dict_key, $ui);
+        if ($array_msg[0] == '/full')
+        {
+            $reply = full_output($array_msg[1], $article);
+        }
+        elseif ($array_msg[0] == '/short')
+        {
+            $reply = short_output_detailed($array_msg[1], $article);
+        }
+        elseif ($array_msg[0] == '/def')
+        {
+            $reply = $array_msg[1].' language is '.lang_def($array_msg[1], $yandex_trans_key);
+        }
+        elseif ($array_msg[0] == '/help')
+        {
+            $reply = "Help message";
+        }
+        else
+        {
+            $reply = "Unknown command";
+        }
+
+    }
+    else
+    {
+        $article = getArticleFromSource($source, $lang, $message, $yandex_dict_key, $ui);
+        $reply = shortest_output($message, $article);
+    }
+    return $reply;
+}
+// Получение статьи из внешнего источника
+function getArticleFromSource($source, $lang, $input_text, $key, $ui)
+{
+    $url = sprintf('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=%s&lang=%s&text=%s&ui=%s&flags=2', $key, $lang, $input_text, $ui);
+    $json_data = file_get_contents($url);
+    return $json_data;
+}
+
+// Определение языка вводимого слова
+function lang_def($message, $key)
+{
+    $url = sprintf('https://translate.yandex.net/api/v1.5/tr.json/detect?hint=en,ru&key=%s&text=%s', $key, $message);
+    $json_data = file_get_contents($url);
+    $data = json_decode($json_data);
+    $reply = $data->lang;
+    return $reply;
+}
+
+// Несколько вариантов перевода
+function full_output($input, $article)
+{
+    $data = json_decode($article);
+    for ($i = 0; $i<=4; $i++) {
+        $trans[$i] = $data->def[0]->tr[$i]->text;
+    }
+    $transfiltered = array_filter ($trans);
+    $reply = 'The word "'.$input.'" translates like: '.implode(', ', $transfiltered).'.';
+    return $reply;
+}
+
+// Один Вариант перевода подробный (с частью речи и синонимами, если есть)
+function short_output_detailed($input, $article)
+{
+    $data = json_decode($article);
+    $trans = $data->def[0]->tr[0]->text;
+    $pos = $data->def[0]->tr[0]->pos;
+    $syn = $data->def[0]->tr[0]->syn[0]->text;
+    if (empty($syn))
+    {
+        $reply = $trans.' ('.$pos.').';
+    }
+    else
+    {
+        $syn_pos = $data->def[0]->tr[0]->syn[0]->pos;
+        $reply = $trans.' ('.$pos.'), синоним - '.$syn.'. ('.$syn_pos.').';
+    }
+    return $reply;
+}
+
+// Один Вариант перевода (самый короткий, просто перевод)
+function shortest_output($input, $article)
+{
+    $data = json_decode($article);
+    $reply = $data->def[0]->tr[0]->text;
+    return $reply;
+}
