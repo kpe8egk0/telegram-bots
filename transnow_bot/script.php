45002e175e291e53f2bea405823f7eb2e099a130
ilya.kiselev <ilya.kiselev@hotmail.com>
2016-09-09 14:50:20 +0300

    lookup return

--- transnow_bot/script.php
+++ transnow_bot/script.php
@@ -26,7 +26,7 @@ if (!isset($user['user'])) {
 
 switch ($message) {
     case '/start':
-        sendMessage($chat_id, 'Hi there! Please write any word for translation! (RU or EN)! Привет! Пожалуйста, напиши любое слово для перевода! (RU ли EN)');
+        sendMessage($chat_id, 'Hi there! Please write any word for translation! (RU or EN)! Привет! Пожалуйста, напиши любое слово для перевода! (RU или EN)');
         exit();
     case '/help':
         sendMessage($chat_id, 'Here will be the help message! Someday :)');
