a037ce08ed63c4cef7b3ac82bb1bebcf1e28ea82
Александр Ширяев <shiryaev.aleksandr@otr.ru>
2016-09-09 17:19:16 +0300

    dallas

--- transnow_bot/dallas_test.php
+++ transnow_bot/dallas_test.php
@@ -1,7 +1,7 @@
 <?php
 $json_data = '{"head":{},"def":[{"text":"pain","pos":"noun","ts":"peɪn","tr":[{"text":"боль","pos":"существительное","gen":"ж","syn":[{"text":"страдание","pos":"существительное","gen":"ср"},{"text":"горе","pos":"существительное","gen":"ср"},{"text":"огорчение","pos":"существительное","gen":"ср"},{"text":"мука","pos":"существительное","gen":"ж"},{"text":"мучение","pos":"существительное","gen":"ср"}],"mean":[{"text":"ache"},{"text":"misery"},{"text":"grief"},{"text":"agony"}],"ex":[{"text":"back pain","tr":[{"text":"боль в спине"}]},{"text":"mental pain","tr":[{"text":"душевное страдание"}]},{"text":"pains of hell","tr":[{"text":"муки ада"}]}]},{"text":"обезболивание","pos":"существительное","gen":"ср","mean":[{"text":"anesthesia"}]}]},{"text":"pain","pos":"verb","ts":"peɪn","tr":[{"text":"болеть","pos":"глагол","asp":"несов","mean":[{"text":"hurt"}]},{"text":"мучить","pos":"глагол","asp":"несов","mean":[{"text":"torment"}]},{"text":"стараться","pos":"глагол","asp":"несов","mean":[{"text":"try"}]},{"text":"огорчать","pos":"глагол","asp":"несов","mean":[{"text":"upset"}]}]},{"text":"pain","pos":"adjective","ts":"peɪn","tr":[{"text":"болевой","pos":"прилагательное","mean":[{"text":"painful"}],"ex":[{"text":"pain syndrome","tr":[{"text":"болевой синдром"}]}]},{"text":"больной","pos":"прилагательное","mean":[{"text":"patient"}]},{"text":"болеутоляющий","pos":"прилагательное","mean":[{"text":"painkiller"}],"ex":[{"text":"pain medication","tr":[{"text":"болеутоляющее средство"}]}]}]}]}';
 
-echo sendDetailedOutput($json_data);
+echo sendDetailedOutput_new($json_data);
 
 echo '<br/><br/>';
 
@@ -24,8 +24,8 @@ function sendDetailedOutput($article)
     }
     return $result;
 }
-/*
-function sendDetailedOutput2($article)
+
+function sendDetailedOutput_new($article)
 {
     $result = '';
     $data = json_decode($article);
@@ -34,4 +34,4 @@ function sendDetailedOutput2($article)
     }
 
     return $result;
-}*/
+}
