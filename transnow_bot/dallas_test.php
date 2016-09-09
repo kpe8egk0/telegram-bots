57ab1c427fd28381e6433f49295af72dc9301442
Александр Ширяев <shiryaev.aleksandr@otr.ru>
2016-09-09 16:04:18 +0300

    dallas

--- transnow_bot/dallas_test.php
+++ transnow_bot/dallas_test.php
@@ -1,73 +1,21 @@
 <?php
-header('Content-Type: text/html; charset=UTF-8');
-//echo phpinfo();
+$json_data = '{"head":{},"def":[{"text":"среда","pos":"существительное","gen":"ж","anm":"неодуш","tr":[{"text":"environment","pos":"noun","syn":[{"text":"surroundings","pos":"noun"},{"text":"milieu","pos":"noun"}],"mean":[{"text":"окружающая среда"},{"text":"окружение"}],"ex":[{"text":"морская среда","tr":[{"text":"marine environment"}]},{"text":"естественная среда","tr":[{"text":"natural surroundings"}]},{"text":"городская среда","tr":[{"text":"urban milieu"}]}]},{"text":"Wednesday","pos":"noun","syn":[{"text":"wednesday","pos":"noun"}],"ex":[{"text":"черная среда","tr":[{"text":"black wednesday"}]}]},{"text":"medium","pos":"noun","mean":[{"text":"носитель"}],"ex":[{"text":"питательная среда","tr":[{"text":"nutrient medium"}]}]},{"text":"midst","pos":"noun","mean":[{"text":"разгар"}]},{"text":"framework","pos":"noun","mean":[{"text":"основа"}],"ex":[{"text":"среда разработки","tr":[{"text":"development framework"}]}]},{"text":"environs","pos":"noun"}]}]}';
 
-$key = 'dict.1.1.20160819T080857Z.a21f9f5c92e0e7b9.ab24906e2b9b24a62bede201ca3067abadaf5752';
-$input_text = 'fuck';
-$langs = 'en-ru';
-//$url = sprintf('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=%s&lang='.$langs.'&text='.$input_text, $key);
-$url = sprintf('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=%s&lang=' . $langs . '&text=' . $input_text, $key);
-$json_data = file_get_contents($url);
-// echo $json_data;
+echo sendDetailedOutput($json_data);
 
-
-function db()
+function sendDetailedOutput($article)
 {
-    define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-    define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-    define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-    define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-    define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
-    $dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';port=' . DB_PORT;
-    $dbh = new PDO($dsn, DB_USER, DB_PASS);
-    return $dbh;
-}
+    $data = json_decode($article);
+    $trans = $data->def[0]->tr[0]->text;
+    $pos = $data->def[0]->tr[0]->pos;
+    $syn = $data->def[0]->tr[0]->syn[0]->text;
+    //Если синонима нет, выводим просто перевод и часть речи
+    if (empty($syn)) {
+        $result = $trans . ' (' . $pos . ').';
+    } else {
+        $syn_pos = $data->def[0]->tr[0]->syn[0]->pos;
+        $result = $trans . ' (' . $pos . '), synonym - ' . $syn . ' (' . $syn_pos . ').';
 
-function sendMessage($chat_id, $message)
-{
-    file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
-}
-
-// Получение статьи из БД
-function getArticleFromDB($source, $lang, $input_text)
-{
-    $db = db();
-    $stmt = $db->prepare('SELECT article FROM article WHERE input_text = :input_text AND lang_type_code = :lang'); //
-    $stmt->bindParam(':input_text', $input_text);
-    $stmt->bindParam(':lang', $lang);
-    $stmt->execute();
-    echo $stmt->rowCount() . '<br/>';
-    $row = $stmt->fetch();
-
-
-    $json_data = $row['article'];
-
-    return $json_data;
-}
-
-// Получение статьи из внешнего источника
-function getArticleFromSource($source, $lang, $input_text, $key)
-{
-    $url = sprintf('https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=%s&lang=%s&text=%s', $key, $lang, $input_text);
-    $json_data = file_get_contents($url, TRUE);
-    return $json_data;
-}
-
-$message = 'dd';
-$lang = 'ru-en';
-
-//Попытка получить статью из БД
-$article_from_db = getArticleFromDB($source, $lang, $message);
-if ($article_from_db != NULL) {
-    // Если получена статья из БД
-    echo 'pass';
-    echo '<br/>';
-    echo $article_from_db;
-} else {
-    // Если не получена  статья из БД
-    // Попытка получить статью из внешнего источника
-    $article_from_source = getArticleFromSource($source, $lang, $message, $yandex_key);
-    echo 'pass 2';
-    echo '<br/>';
-    var_dump($article_from_source);
+    }
+    return $result;
 }
