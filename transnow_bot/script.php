3f00a8339f62b0ed4346b6466917471b1b87ea5c
ilya.kiselev <ilya.kiselev@hotmail.com>
2016-09-09 15:54:50 +0300

    help-ru, help-en

--- transnow_bot/script.php
+++ transnow_bot/script.php
@@ -28,7 +28,10 @@ switch ($message) {
     case '/start':
         sendMessage($chat_id, 'Hi there! Please write any word for translation! (RU or EN)! Привет! Пожалуйста, напиши любое слово для перевода! (RU или EN)');
         exit();
-    case '/help':
+    case '/help-ru':
+        sendMessage($chat_id, 'Здесь будет сообщение с помощью! Когда-нибудь :)');
+        exit();
+    case '/help-en':
         sendMessage($chat_id, 'Here will be the help message! Someday :)');
         exit();
     case '/lepra':
