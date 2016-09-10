6a589d83593eabf6bcc7945dac39976e0a4a1cf3
alexs <alexshir83@gmail.com>
2016-09-11 00:36:07 +0300

    dallas

--- transnow_bot/stats.php
+++ transnow_bot/stats.php
@@ -46,7 +46,7 @@ function getLookupLastN($n) {
 
     $output = '';
     foreach ($stmt as $row) {
-        $output = $output . '<br/>[' . $row['date'] . '] ' . $row['user'] . ': "' . $row['input_text'] . '"';
+        $output = $output . '<br/>[' . $row['lookup.date'] . '] ' . $row['lookup.user'] . ': "' . $row['lookup.input_text'] . '"';
     }
 
     return $output;
