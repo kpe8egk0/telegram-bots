997e95e2f9c9a93c7cfbbf345719f324ba8a1273
Aleksandr Shirayev <shiryaev.aleksandr@otr.ru>
2017-01-23 12:26:15 +0300

    1

--- application/models/chartmodel.php
+++ application/models/chartmodel.php
@@ -13,14 +13,17 @@ class ChartModel extends CI_Model {
     private $performance = 'performance';
 
     function __construct() {
-        $this->load->database();
+        //$this->load->database();
     }
 
     function get_chart_data() {
 
+        /*
         $stmt = $this->db->prepare('SELECT date(date) as day, COUNT(id) AS qty FROM lookup GROUP BY date(date) DESC');
         $stmt->execute();
         $results['chart_data'] = $stmt->fetchAll();
+        */
+        $results['chart_data'] = array(array('day' => '1', 'qty' => 6), array('day' => '2', 'qty' => 3));
 
         return $results;
     }
